b717ba67c759431bedd8ecc97fa8abb401042d44
Andriy0124 <katuhaandrij696@gmail.com>
2025-05-24 00:54:26 +0300

    Початковий коміт

---
+++ src/assets/js/languages.js
@@ -0,0 +1,63 @@
+export const languages = {
+  en: {
+    Screenshots: "Screenshots", 
+    editions: "Editions",
+    "editions-title": "Editions",
+    "standard-edition": "Standard Edition",
+    "platform-pc": "PC",
+    controller: "Controller",
+    about: "About",
+    explore: "Explore",
+    news: "News",
+    faq: "FAQ",
+    gameTitle: "S.T.A.L.K.E.R.2",
+    gameDescription: `Three years have passed since the last major emission. The winter — nicknamed Fimbulwinter by stalkers — lasted three summers long. Cold, radiation, and mutated creatures have become the new norm. But something is changing… The weather grows unstable, artifacts behave erratically, and detectors fail more often. Old legends of Ragnarok, the end of all things, are spreading through the Zone again. Those still alive are preparing — for the darkest days are yet to come.`,
+    buyNow: "Buy now",
+    heartOfChornobyl: "THE HEART OF CHORNOBYL",
+    gameIntro: `
+      The Heart of the Zone beats faster... Its pulse echoes across Chernobyl’s ashes, disturbing the silence and summoning new blood. It calls once more to stalkers — those fearless enough to enter the depths of its eerie core, where true tests of will, mind, and spirit await.
+      S.T.A.L.K.E.R. 2: Heart of Chornobyl isn’t just a game. It’s your new life. A life in a shattered world, where every step could be your last. Unreal Engine and photogrammetry have brought the Zone to life, making it so real that you won’t feel the line between yourself and what lies beyond the rusted gates.
+      And don’t think enemies are the only threat. The AI doesn’t forgive mistakes. It learns, adapts, and hunts. But the worst part? True struggle begins when bullets run dry, food is gone, and trust in your comrades crumbles like dust in the storm.
+      Here, it’s just you — and the Zone. And she always plays by her own rules.
+    `,
+    screenshots: "Game Screenshots",
+    main: "Main game",
+    bonus: "Bonus side quest",
+digital: "Digital Artbook",
+official: "Official Soundtrack",
+costume: "3 unique outfits, 6 exclusive weapons",
+costume1: "2 special outfits and 3 unique weapons (transferable to multiplayer)",
+seasonal: "Season Pass (2 story expansions and all future DLCs)",
+
+  },
+  ua: {
+    Screenshots: "Скріншоти", 
+    editions: "Видання",
+    "editions-title": "Видання",
+    "standard-edition": "Стандартне видання",
+    "platform-pc": "ПК",
+    controller: "Контролер",
+    about: "Про гру",
+    explore: "Досліджувати",
+    news: "Новини",
+    faq: "FAQ",
+    gameTitle: "S.T.A.L.K.E.R.2",
+    gameDescription: `Минуло три роки з останнього великого вибуху. Зима, яку сталкери прозвали Фімбулвінтером, тривала три літа. Холод, радіація та мутовані істоти стали новою нормою. Але щось змінюється... Погода стає нестабільною, артефакти поводяться дивно, а детектори частіше дають збої. Старі легенди про Рагнарок, кінець всього сущого, знову поширюються по Зоні. Ті, хто ще живі, готуються — найтемніші дні ще попереду.`,
+    buyNow: "Придбати",
+    heartOfChornobyl: "СЕРЦЕ ЧОРНОБИЛЯ",
+    gameIntro: `
+    Серце Зони б’ється частіше... Луна цього пульсу розноситься згарищами Чорнобиля, стираючи спокій і притягаючи нову кров. Вона знову кличе сталкерів — тих, хто не боїться піти вглиб її моторошного нутра, туди, де починається справжнє випробування волі, розуму і духу.
+    S.T.A.L.K.E.R. 2: Серце Чорнобиля — не просто гра. Це твоє нове життя. Життя у зруйнованому світі, де кожен крок може стати останнім. Unreal Engine і фотограмметрія оживили Зону, зробили її настільки реалістичною, що ти не відчуєш межі між собою і тим, що чекає за ржавими воротами.
+    І не думай, що вороги — єдина загроза. Штучний інтелект тут не прощає помилок. Він вивчає, пристосовується, нищить. Але найгірше — не це. Справжні труднощі починаються, коли закінчуються патрони, їжа, а довіра до напарників розсипається, як пил у буревії.
+    Тут ти — лише один проти Зони. І вона завжди грає за своїми правилами.
+  `,
+  screenshots: "Скріншоти з гри",
+  main: "Основна гра",
+  bonus:" Бонусний побічний квест",
+  digital:" Цифровий Артбук",
+  official:"  Офіційний Саундтрек",
+  costume:" 3 особливий костюм, 6 унікальних одиниці зброї",
+  costume1:" Особливі костюми 2 та унікальна зброя 3 (переноситься у багатокористувацький режим)",
+  seasonal:" Сезонний абонемент (2 сюжетні доповнення та всі майбутні DLC)",
+},
+}
